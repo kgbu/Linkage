b8dbeeb95a6d963e5f944007ab37e7e0f390a7b8
HPMSD Kazutaka Ohgaki <ogaki@hpmsd.co.jp>
2013-11-11 11:59:42 +0900

    status quo

--- src/looper.erl
+++ src/looper.erl
@@ -1,5 +1,6 @@
 
-
+%% @doc 
+%% @end
 
 -module(looper).
 
@@ -9,17 +10,31 @@
 -export([run/0]).
 
 run() ->
-	Pid = spawn(fun ping/0),
-	Pid ! self(),
-	receive
-		pong -> okk
-	end.
+	PidLists = spawn_procs([] | 1000).
+
+%% spawn whole neurons
+spawn_procs(L, 0) ->
+	L;
+spawn_procs(L, N) when_is_list(L), when_is_number(N) ->
+	Pid = spawn(fun newron/1 [N]),
+	spawn_procs([Pid|L], N - 1).
+
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
-
-ping() ->
+newron(N) ->
+	{Neighbour, Flags} = State,
 	receive
-		From -> From ! pong
+		{neighbor, ID} ->
+			ID ! Neighbour,
+			newron(State)
+			;
+		{add, Path} ->
+			{Neighbour, Flags} = State
+			newron([Path| Neighbour],Flags)
+			;
+		{remove, ID} ->
+			newron(lists:delete(ID, Neighbor), Flags)
 	end.
+
--- src/synapsepool.erl
+++ src/synapsepool.erl
@@ -9,10 +9,38 @@
 
 
 run(Type) ->
-	{ok, Type}.
+	Pool = setuppool([], 10000),
+	{ok, Pool}.
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
+setuppool(L, 0)->
+		L.
+setuppool(L, N) when_is_list(L), when_is_number(N) ->
+		spawn(fun syapse/1 [[],init])
+		.
 
+synapse(State) ->
+    {Neighbor, Flags} = State,
+	receive
+		{neighbor, ID} ->
+			ID ! {self(),Neighbor},
+			synapse(State)
+			;
+		{add, Path} ->
+			synapse([Path | Neighbor],Flags)
+			;
+		{remove, ID} -> 
+			synapse(lists:delete(ID, Neighbor), Flags)
+			;
+	after 10000 ->
+		%% after 10000 msec, decrese list of neighbors
+		synapse(shurink(Neighbors), Flags)
+	end.
+
+shurink([]) -> 
+	[];
+shurink([H|L]) ->
+	L.
